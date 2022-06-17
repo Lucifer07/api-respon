53f6b36ff512237288b063bfab230d0b8e904f60
Lucifer07 <bagas1615@gmail.com>
2022-06-17 08:58:10 +0700

    jadi6

--- hello.py
+++ hello.py
@@ -1,75 +1,141 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import string
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+from keras.models import load_model
 import random
 import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
 from flask_cors import CORS
 from flask import abort, Flask, jsonify, redirect, request, url_for
-f = open('data.txt', 'r', errors='ignore')
-raw = f.read()
-raw = raw.lower()
-sentence_list = nltk.sent_tokenize(raw)
-word_tokens = nltk.word_tokenize(raw)
-lemmer = nltk.stem.WordNetLemmatizer()
-def LemTokens(tokens):
-    return [lemmer.lemmatize(token) for token in tokens]
-remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
-def LemNormalize(text):
-    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
-# Salam Pembuka
-salam_input_user = ("hello", "hi", "yuhuy", "oiy", "hey")
-salam_respon_bot = ["hi", "hello", "yuhuy", "oit", "hey"]
+try:
+    model = load_model('chatbot_model.h5')
+    intents = json.loads(open('intents.json').read())
+    words = pickle.load(open('words.pkl','rb'))
+    classes = pickle.load(open('classes.pkl','rb'))
+except:
+    words=[]
+    classes = []
+    documents = []
+    ignore_letters = ['!', '?', ',', '.']
+    intents_file = open('intents.json').read()
+    intents = json.loads(intents_file)
+    for intent in intents['intents']:
+        for pattern in intent['patterns']:
+            #tokenize each word
+            word = nltk.word_tokenize(pattern)
+            words.extend(word)
+            #add documents in the corpus
+            documents.append((word, intent['tag']))
+            # add to our classes list
+            if intent['tag'] not in classes:
+                classes.append(intent['tag'])
+    words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_letters]
+    words = sorted(list(set(words)))
+    classes = sorted(list(set(classes)))
+    pickle.dump(words,open('words.pkl','wb'))
+    pickle.dump(classes,open('classes.pkl','wb'))
+    training = []
+    output_empty = [0] * len(classes)
+    for doc in documents:
+        bag = []
+        pattern_words = doc[0]
+        pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+        for word in words:
+            bag.append(1) if word in pattern_words else bag.append(0)
+        output_row = list(output_empty)
+        output_row[classes.index(doc[1])] = 1
+        training.append([bag, output_row])
+    random.shuffle(training)
+    training = np.array(training)
+    train_x = list(training[:,0])
+    train_y = list(training[:,1])
+    model = Sequential()
+    model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(train_y[0]), activation='softmax'))
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy']) 
+    hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+    model.save('chatbot_model.h5', hist)
+finally:
+    def clean_up_sentence(sentence):
+        # tokenize the pattern - splitting words into array
+        sentence_words = nltk.word_tokenize(sentence)
+        # stemming every word - reducing to base form
+        sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+        return sentence_words
+    # return bag of words array: 0 or 1 for words that exist in sentence
+    def bag_of_words(sentence, words, show_details=True):
+        # tokenizing patterns
+        sentence_words = clean_up_sentence(sentence)
+        # bag of words - vocabulary matrix
+        bag = [0]*len(words)  
+        for s in sentence_words:
+            for i,word in enumerate(words):
+                if word == s: 
+                    # assign 1 if current word is in the vocabulary position
+                    bag[i] = 1
+                    if show_details:
+                        print ("found in bag: %s" % word)
+        return(np.array(bag))
 
-def greeting(scentence):
+    def predict_class(sentence):
+        # filter below  threshold predictions
+        p = bag_of_words(sentence, words,show_details=False)
+        res = model.predict(np.array([p]))[0]
+        ERROR_THRESHOLD = 0.25
+        results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+        # sorting strength probability
+        results.sort(key=lambda x: x[1], reverse=True)
+        return_list = []
+        for r in results:
+            return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+        return return_list
 
-    for word in scentence.split():
-        if word.lower() in salam_input_user:
-            return random.choice(salam_respon_bot)
-# Respon
-def response(user_response):
-    chatbot_response = ''
-    sentence_list.append(user_response)
-    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words="english")
-    tfidf = TfidfVec.fit_transform(sentence_list)
-    vals = cosine_similarity(tfidf[-1], tfidf)
-    idx = vals.argsort()[0][-2]
-    flat = vals.flatten()
-    flat.sort()
-    req_tfidf = flat[-2]
-    if(req_tfidf == 0):
-        chatbot_response = chatbot_response + "Maaf, kami tidak bisa mengerti pertanyaan anda"
-        return chatbot_response
-
-    else:
-        chatbot_response = chatbot_response+sentence_list[idx]
-        return chatbot_response
-
-def response_api(data):
-    return (
-        jsonify(**data),
-        data['code']
-    )
-app = Flask(__name__)
-CORS(app)
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World! Deploy nich...</p>"
-@app.route('/chat', methods=['POST'])
-def chat():
-    if request.method == 'POST':
-        data = request.form['message']
-        hasil=response(data)
-        if(data==hasil):
-            hasil="Maaf, kami tidak bisa mengerti pertanyaan anda"
-        return response_api({
-            'code': 200,
-            'message': 'Berhasil',
-            'data': hasil
-        })
-    else:
-        return response_api({
-            'code': 400,
-            'message': 'Gagal',
-            'data': 'Tidak dapat mengakses'
-        })
+    def getResponse(ints, intents_json):
+        tag = ints[0]['intent']
+        list_of_intents = intents_json['intents']
+        for i in list_of_intents:
+            if(i['tag']== tag):
+                result = random.choice(i['responses'])
+                break
+        return result
+    def response_api(data):
+        return (
+            jsonify(**data),
+            data['code']
+        )
+    model = load_model('chatbot_model.h5')
+    intents = json.loads(open('intents.json').read())
+    words = pickle.load(open('words.pkl','rb'))
+    classes = pickle.load(open('classes.pkl','rb'))
+    app = Flask(__name__)
+    CORS(app)
+    @app.route("/")
+    def hello_world():
+        return "<p>Hello, World! Deploy nich...</p>"
+    @app.route('/chat', methods=['POST'])
+    def chat():
+        if request.method == 'POST':
+            data = request.form['message']
+            ints = predict_class(data)
+            res = getResponse(ints, intents)
+            if(data==res):
+                res="Maaf, kami tidak bisa mengerti pertanyaan anda"
+            return response_api({
+                'code': 200,
+                'message': 'Berhasil',
+                'data': res
+            })
+        else:
+            return response_api({
+                'code': 400,
+                'message': 'Gagal',
+                'data': 'Tidak dapat mengakses'
+            })
